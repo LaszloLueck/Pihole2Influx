fb2033d72488b8bedafcd0b030f23988f7fe06b6
Laszlo Lueck <llueck@gmail.com>
2020-09-20 17:09:52 +0200

    add logging

---
+++ dck_pihole2influx/Logging/LoggingFactory.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace dck_pihole2influx.Logging
+{
+    public static class LoggingFactory<T>
+    {
+
+        public static ILogger CreateLogging()
+        {
+            return new LoggerConfiguration()
+                .WriteTo
+                .Console(theme: AnsiConsoleTheme.Code,
+                    outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}")
+                .CreateLogger()
+                .ForContext<T>();
+        }
+        
+    }
+}
--- dck_pihole2influx/Program.cs
+++ dck_pihole2influx/Program.cs
@@ -1,22 +1,19 @@
-﻿using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using dck_pihole2influx.Configuration;
-using log4net;
-using log4net.Config;
+using dck_pihole2influx.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace dck_pihole2influx
 {
     class Program
     {
-        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+        private static readonly ILogger Log = LoggingFactory<Program>.CreateLogging();
 
         static async Task Main(string[] args)
         {
-            //BasicConfigurator.Configure();
-            XmlConfigurator.Configure(new FileInfo("LoggerConfig.xml"));
-            
-            
-            Logger.Info("starting app!");
+            Log.Information("starting app!");
             
             RunThings();
 
@@ -26,24 +23,14 @@ namespace dck_pihole2influx
         private static void RunThings()
         {
             var configuration = new ConfigurationFactory().Configuration;
-            Logger.Info("Use the following parameter for connections:");
-            Logger.Info($"Pihole host: {configuration.PiholeHostOrIp}");
-            Logger.Info($"Pihole telnet port: {configuration.PiholeTelnetPort}");
-            Logger.Info($"InfluxDb host: {configuration.InfluxDbHostOrIp}");
-            Logger.Info($"InfluxDb port: {configuration.InfluxDbPort}");
-            Logger.Info($"InfluxDb database name: {configuration.InfluxDbDatabaseName}");
-            Logger.Info($"InfluxDb user name: {configuration.InfluxDbUserName}");
-            Logger.Info($"InfluxDb password is {(configuration.InfluxDbPassword.Length == 0 ? "not set" : "set")}");
-
-            //Console.WriteLine("Use the following parameter for connections:");
-            //Console.WriteLine($"Pihole host: {configuration.PiholeHostOrIp}");
-            //Console.WriteLine($"Pihole telnet port: {configuration.PiholeTelnetPort}");
-            //Console.WriteLine($"InfluxDb host: {configuration.InfluxDbHostOrIp}");
-            //Console.WriteLine($"InfluxDb port: {configuration.InfluxDbPort}");
-            //Console.WriteLine($"InfluxDb database name: {configuration.InfluxDbDatabaseName}");
-            //Console.WriteLine($"InfluxDb user name: {configuration.InfluxDbUserName}");
-            //Console.WriteLine("InfluxDb password is {0}",
-            //    configuration.InfluxDbPassword.Length == 0 ? "not set" : "set");
+            Log.Information("Use the following parameter for connections:");
+            Log.Information($"Pihole host: {configuration.PiholeHostOrIp}");
+            Log.Information($"Pihole telnet port: {configuration.PiholeTelnetPort}");
+            Log.Information($"InfluxDb host: {configuration.InfluxDbHostOrIp}");
+            Log.Information($"InfluxDb port: {configuration.InfluxDbPort}");
+            Log.Information($"InfluxDb database name: {configuration.InfluxDbDatabaseName}");
+            Log.Information($"InfluxDb user name: {configuration.InfluxDbUserName}");
+            Log.Information($"InfluxDb password is {(configuration.InfluxDbPassword.Length == 0 ? "not set" : "set")}");
         }
     }
 }
