096ea25bd85c0cbbbf7c2dc025feb5570d5ba525
Laszlo Lueck <llueck@gmail.com>
2020-11-30 17:48:47 +0100

    add measurement of telnet errors to influxdb

---
+++ dck_pihole2influx/Transport/InfluxDb/Measurements/MeasurementTelnetReadError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using InfluxDB.Client.Core;
+
+namespace dck_pihole2influx.Transport.InfluxDb.Measurements
+{
+    [Measurement("telnetReadError")]
+    public class MeasurementTelnetReadError : IBaseMeasurement
+    {
+#pragma warning disable
+        [Column(IsTimestamp = true)] public DateTime Time;
+#pragma warning restore
+        
+        
+    }
+}
---
+++ dck_pihole2influx/Transport/InfluxDb/Measurements/MeasurementTelnetWriteError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InfluxDB.Client.Core;
+
+namespace dck_pihole2influx.Transport.InfluxDb.Measurements
+{
+    [Measurement("telnetWriteError")]
+    public class MeasurementTelnetWriteError : IBaseMeasurement
+    {
+#pragma warning disable
+        [Column(IsTimestamp = true)] public DateTime Time;
+#pragma warning restore
+        [Column("writeErrorType")] public string WriteErrorType { get; set; } 
+        
+        
+    }
+}
